79887cb6fa50fa178fcb2b5b567560fc0bc02f17
ssavrim <ssavrimo@gmail.com>
2016-06-01 13:39:49 +0200

    Update arduino_extension.js

--- arduino_extension.js
+++ arduino_extension.js
@@ -179,6 +179,7 @@
   }
 
   function processSysexMessage() {
+    console.log(storedInputData[0]);
     switch(storedInputData[0]) {
       case CAPABILITY_RESPONSE:
         for (var i = 1, pin = 0; pin < MAX_PINS; pin++) {
